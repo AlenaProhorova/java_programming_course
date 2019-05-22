c25ccc18455cade9bfdb6f77a3a48f0dd50b25f6
AlenaProhorova <a.e.prohorova@gmail.com>
2019-05-23 00:09:04 +0400

    Задание 2. Distance Point

---
+++ sandbox/src/main/java/java_prog/first/sandbox/PointsDistance.java
@@ -0,0 +1,11 @@
+package java_prog.first.sandbox;
+
+public class PointsDistance {
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point(1,11);
+        System.out.println("Расстояние между точками =" + p1.distance(2,22));
+    }
+
+}
